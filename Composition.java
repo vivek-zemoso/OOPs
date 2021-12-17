12d9eeffd8e2320d490f72168d426a2e534889b6
Vivek Jain <nectars.vivek@gmail.com>
2021-12-17 16:37:36 +0530

    Composition Example

---
+++ Composition.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Car {
+    private String name;
+    private int price;
+    private Engine engine;
+
+    public Car(String name, int price, String e_no, int hp) {
+        this.name = name;
+        this.price = price;
+        this.engine = new Engine(e_no, hp);
+    }
+
+    public void displayCarInfo() {
+        System.out.println("Name " + this.name);
+        System.out.println("Age " + this.price);
+        System.out.println("Engine :- ");
+        engine.displayEngineInfo();
+    }
+}
+
+class Engine {
+    private String engine_no;
+    private int horsePower;
+
+    public Engine(String engine_no, int horsePower) {
+        this.engine_no = engine_no;
+        this.horsePower = horsePower;
+    }
+
+    public void displayEngineInfo() {
+        System.out.println("Engine Number - " + this.engine_no);
+        System.out.println("Horse Power - " + this.horsePower + " BHP");
+    }
+}
+
+public class Composition {
+
+    public static void main(String[] args) {
+
+        Car p = new Car("Vivek Jain", 21, "DL_A65_0XLP_5644", 750);
+        p.displayCarInfo();
+    }
+}
